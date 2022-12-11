ba6337e5ecddb61a652dc9524977602121b8c8db
Ihor Fesina <warriork123456@gmail.com>
2022-12-11 11:50:32 +0100

    create a wrapper

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,14 @@
-import './index.css'
-// import { App2 } from './App2'
-// import App from './App'
-import { App3 } from './App3'
+import './style.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
-  // <React.StrictMode>
-  // <App />
-  // <App2 />
-  <>
-    <App3 />
-    {/* <App3 />
-    <App3 /> */}
-  </>
-  // </React.StrictMode>
+  <div className='wrapper'>
+
+</button>
+  </div>
 )
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+
